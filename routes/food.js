07a3fa26ebb11ea09cad8767b219fb5c2fc9caab
Jeremy Twidt <jtwidt@gmail.com>
2024-01-28 15:09:54 -0600

    basic route templates in place

---
+++ routes/food.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.status(200).send({
+        message: `This is the root route for the Food endpoints`,
+    });
+});
+
+module.exports = router;
