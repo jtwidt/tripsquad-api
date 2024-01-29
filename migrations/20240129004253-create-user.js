fa56fcfde50425cd8534e4687a6b30d778fd1bc5
Jeremy Twidt <jtwidt@gmail.com>
2024-01-28 19:03:52 -0600

    User model creation

---
+++ migrations/20240129004253-create-user.js
@@ -0,0 +1,54 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('Users', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            firstName: {
+                allowNull: false,
+                type: Sequelize.STRING,
+            },
+            lastName: {
+                allowNull: false,
+                type: Sequelize.STRING,
+            },
+            email: {
+                type: Sequelize.STRING,
+            },
+            address1: {
+                type: Sequelize.STRING,
+            },
+            address2: {
+                type: Sequelize.STRING,
+            },
+            city: {
+                type: Sequelize.STRING,
+            },
+            state: {
+                type: Sequelize.STRING,
+            },
+            country: {
+                type: Sequelize.STRING,
+            },
+            postCode: {
+                type: Sequelize.STRING,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('Users');
+    },
+};
---
+++ models/user.js
@@ -0,0 +1,32 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class User extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    }
+    User.init(
+        {
+            firstName: { type: DataTypes.STRING, allowNull: false },
+            lastName: { type: DataTypes.STRING, allowNull: false },
+            email: DataTypes.STRING,
+            address1: DataTypes.STRING,
+            address2: DataTypes.STRING,
+            city: DataTypes.STRING,
+            state: DataTypes.STRING,
+            country: DataTypes.STRING,
+            postCode: DataTypes.STRING,
+        },
+        {
+            sequelize,
+            modelName: 'User',
+        }
+    );
+    return User;
+};
