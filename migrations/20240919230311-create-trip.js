1d54e38c8bed98fb2a843f8bf771b4db095f2e6b
Jeremy Twidt <jtwidt@gmail.com>
2024-09-19 18:17:27 -0500

    first attempt at Trip model and migration

---
+++ migrations/20240919230311-create-trip.js
@@ -0,0 +1,48 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('trips', {
+      id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        primaryKey: true,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      trip_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      start_date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      end_date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      location: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      status: {
+        type: Sequelize.ENUM('planned', 'ongoing', 'completed', 'canceled'), // ENUM definition in migration
+        allowNull: false,
+        defaultValue: 'planned',
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('trips');
+    await queryInterface.sequelize.query(
+      'DROP TYPE IF EXISTS "enum_trips_status";'
+    );
+  },
+};
