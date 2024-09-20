26016770ee520a5cae5c700a86e6de8ba2132a22
Jeremy Twidt <jtwidt@gmail.com>
2024-09-20 11:12:17 -0500

    logged in user controller and route

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -5,11 +5,11 @@ const User = db.User;
 // CREATE A USER
 const createUser = async (req, res) => {
   // Gets the clerk user ID provided by the Clerk middleware
-  const { userId } = req.auth;
+  const { clerkId } = req.auth;
 
   // Searches the database for a user with the given Clerk ID, creates the user if none found.
   const [user, created] = await User.findOrCreate({
-    where: { clerkId: userId },
+    where: { clerkId },
   });
 
   // Send an error message if a user was already found or success if the user was created
@@ -17,12 +17,32 @@ const createUser = async (req, res) => {
     return res.status(400).send({ message: 'User already exists' });
   }
 
+  return res.status(201).send({ user });
+};
+
+// GET LOGGED IN USER
+const getLoggedInUser = async (req, res) => {
+  // Gets the clerk user ID provided by the Clerk middleware
+  const { clerkId } = req.auth;
+
+  // Find the user based on the Clerk ID
+  const user = await User.findOne({ where: { clerkId } });
+
+  // Return an error if the user isn't found
+  if (!user) {
+    return res.status(400).send({ message: 'User not found' });
+  }
+
+  // Return the found user
   return res.status(200).send({ user });
 };
 
-// GET A SINGLE USER
-
 // GET ALL USERS
+const getAllUsers = async (req, res) => {
+  const users = await User.findAll({});
+
+  return res.status(200).send({ users });
+};
 
 // UPDATE A USER
 
@@ -30,4 +50,5 @@ const createUser = async (req, res) => {
 
 module.exports = {
   createUser,
+  getLoggedInUser,
 };
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,10 +2,14 @@ const express = require('express');
 const router = express.Router();
 const { requireAuth } = require('@clerk/clerk-sdk-node');
 
-const { createUser } = require('../controllers/user.controller');
+const {
+  createUser,
+  getLoggedInUser,
+} = require('../controllers/user.controller');
 
 router.get('/', (req, res) => {
   return res.status(200).send({ message: 'Users root route' });
 });
 
 router.post('/register', requireAuth(), createUser);
+router.get('/me', requireAuth(), getLoggedInUser);
