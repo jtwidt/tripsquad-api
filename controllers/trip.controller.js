7e65371acafcc88d04c52c139f7840e6ce730a82
Jeremy Twidt <jtwidt@gmail.com>
2024-09-20 12:54:30 -0500

    initial layout of trip controller and route files

---
+++ controllers/trip.controller.js
@@ -0,0 +1,23 @@
+const db = require('../models');
+
+const Trip = db.Trip;
+
+// CREATE A TRIP
+
+// GET A SPECIFIC TRIP
+
+// GET CREATED TRIPS
+
+// GET ATTENDING TRIPS
+
+// GET ALL TRIPS
+
+// UPDATE TRIP
+
+// MODIFY LOCATIONS FOR TRIP
+
+// MODIFY TRIP PARTICIPANTS
+
+// DELETE TRIP
+
+module.exports = {};
---
+++ routes/trip.routes.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router = express.Router();
+const { requireAuth } = require('@clerk/clerk-sdk-node');
+
+const {} = require('../controllers/trip.controller');
