7f38d96054b9c804cb02b65f35ffce8c1bdc2333
Jeremy Twidt <jtwidt@gmail.com>
2024-09-19 19:24:29 -0500

    first attempt at itinerary items model and migration

---
+++ migrations/20240920002047-create-itinerary-item.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('itinerary_items', {
+      id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        primaryKey: true,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      type: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      start_time: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      confirmation_number: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('itinerary_items');
+  },
+};
