ef418d08d1161b4809ef6190a4e5a0c7ea8376df
jtwidt <64605553+jtwidt@users.noreply.github.com>
2024-09-23 13:03:34 -0500

    deleteFlight complete

--- controllers/flight.controller.js
+++ controllers/flight.controller.js
@@ -86,10 +86,112 @@ const addFlightToTrip = async (req, res) => {
 };
 
 // UPDATE FLIGHT
-const updateFlight = async (req, res) => { };
+const updateFlight = async (req, res) => {
+  // Get the Clerk Id for the logged on user
+  const {userId: clerkId} = req.auth;
 
-// DELETE FLIGHT
-const deleteFlight = async (req, res) => { };
+  // Get the flight ID from the URL
+  const {flightId} = req.params;
+
+  // Get the user with the matching Clerk ID
+  const user = await User.findOne({where: {clerkId}});
+
+  // If no user is found return an error message
+  if (!user) {
+    return res.status(400).send({message: 'No user found'})
+  }
+  
+  // Get the flight with the matching flight ID
+  let flight = await Flight.findOne({ where: {id: flightId}});
+
+  // If no flight is found, return an error
+  if (!flight) {
+    return res.status(400),=.send({message: 'No flight found'})
+  }
+
+  // The logged in user is not the person that "owns" the flight
+  if (flight.userId !== user.id) {
+    return res.status(403).send({message: 'Not authorized'});
+  }
+
+  // Get the update info from the request body
+  let {
+    departureTime,
+    arrivalTime,
+  } = req.body;
+
+  // Convert the dates to JS date objects
+  arrivalTime = new Date(arrivalTime);
+  departureTime = new Date(departureTime);
+
+  const updateObject = {...req.body, departureTime, arrivalTime};
+
+  // Update the flight in the database
+  await Flight.update(updateObject, { where: {id: flightId}})
+
+  // Re-fetch the updated object
+  flight = await Flight.findOne({ 
+    where: { 
+      id: flight.id 
+    }, 
+    attributes: { 
+      exclude: ['createdAt', 'updatedAt'] 
+    },
+    include: [
+      {
+        model: User,
+        as: 'flyer',
+        attributes: {
+          exclude: ['createdAt', 'updatedAt']
+        }
+      },
+      {
+        model: Trip,
+        as: 'trip',
+        attributes: {
+          exclude: ['createdAt', 'updatedAt']
+        }
+      }
+    ]
+  });
+
+  return res.status(200).send({flight});
+};
+
+
+ // DELETE FLIGHT
+const deleteFlight = async (req, res) => { 
+  // Get the Clerk ID of the logged in user
+  const {userId: clerkId} = req.auth;
+
+  // Get the flight ID from the URL
+  const {flightId} = req.params;
+
+  // Get the user associated with the Clerk ID
+  const user = await User.findOne({where: {clerkId}});
+
+  // If no user is found, return an error
+  if (!user) {
+    return res.status(400).send({message: 'No user found'});
+  }
+
+  // Get the flight with the matching flight ID
+  const flight = await Flight.findOne({where: {id: flightId}});
+
+  // If no flight is found, return an error
+  if (!flight) {
+    return res.status(400).send({message: 'No flight found'})
+  }
+
+  // If the user is not the owner of the flight return an error
+  if (flight.userId !== user.id) {
+    return res.status(403).send({message: 'Not authorized'});
+  }
+
+  await flight.destroy({where: {id: flightId}});
+
+  return res.status(200).send({message: 'Flight successfully deleted'});
+};
 
 module.exports = {
   addFlightToTrip,
--- routes/flight.routes.js
+++ routes/flight.routes.js
@@ -2,12 +2,14 @@ const express = require('express');
 const router = express.Router();
 const { ClerkExpressWithAuth } = require('@clerk/clerk-sdk-node');
 
-const { addFlightToTrip } = require('../controllers/flight.controller');
+const { addFlightToTrip, updateFlight, deleteFlight } = require('../controllers/flight.controller');
 
 router.get('/', (req, res) => {
   return res.status(200).send({ message: 'Trip root route' });
 });
 
 router.post('/:tripId', ClerkExpressWithAuth(), addFlightToTrip);
+router.patch('/:tripId', ClerkExpressWithAuth(), updateFlight);
+router.delete('/:tripId', ClerkExpressWithAuth(), deleteFlight);
 
 module.exports = router;
