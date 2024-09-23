915d9c83e7074f755d384fc4b11e55d01510dba2
Jeremy Twidt <jtwidt@gmail.com>
2024-09-23 17:24:11 -0500

    finished stubbing out routes

--- routes/location.routes.js
+++ routes/location.routes.js
@@ -3,21 +3,25 @@ const router = express.Router();
 const { ClerkExpressWithAuth } = require('@clerk/clerk-sdk-node');
 
 const {
-    createLocation,
-    getLocationById,
-    getAllLocations,
-    getCountryLocations,
-    getCityLocations,
-    updateLocation,
-    deleteLocation
+  createLocation,
+  getLocationById,
+  getAllLocations,
+  getCountryLocations,
+  getCityLocations,
+  updateLocation,
+  deleteLocation,
 } = require('../controllers/location.controller');
 
 router.get('/', (req, res) => {
-    return res.status(200).send({ message: 'Locations root route' });
-  });
+  return res.status(200).send({ message: 'Locations root route' });
+});
 
-  router.post('/create', ClerkExpressWithAuth(), createLocation);
-  router.get('/all', ClerkExpressWithAuth(), getAllLocations)
-  
+router.post('/create', ClerkExpressWithAuth(), createLocation);
+router.get('/all', ClerkExpressWithAuth(), getAllLocations);
+router.get('/country/:country', ClerkExpressWithAuth(), getCountryLocations);
+router.get('/city/:city', ClerkExpressWithAuth(), getCityLocations);
+router.get('/:id', ClerkExpressWithAuth(), getLocationById);
+router.patch('/:locationId', ClerkExpressWithAuth(), updateLocation);
+router.delete('/:id', ClerkExpressWithAuth(), deleteLocation);
 
-module.exports = router
+module.exports = router;
